93d2eda33a8bbd73d718e3dcc3fcf2a2e2617415
Jamie Norrish <jamie@artefact.org.nz>
2023-09-07 13:28:00 +1200

    Made tracking optional in paired intersector.

--- taclextra/paired_intersector.py
+++ taclextra/paired_intersector.py
@@ -45,21 +45,30 @@ class PairedIntersector:
     def intersect_all(self):
         os.makedirs(self._output_dir, exist_ok=True)
         works = sorted(self._catalogue.keys())
-        with open(self._tracking_path, 'a', newline='') as tracking_fh:
-            writer = csv.writer(tracking_fh)
-            for work in works:
-                output_path = os.path.join(self._output_dir, work)
-                os.makedirs(output_path, exist_ok=True)
-                catalogue = tacl.Catalogue()
-                catalogue[work] = work
-                for alt_work in works:
-                    self.process_pair(work, alt_work, catalogue, output_path,
-                                      writer)
-                # Delete output directory if empty.
-                try:
-                    os.rmdir(output_path)
-                except OSError:
-                    pass
+        if self._tracking_path is not None:
+            with open(self._tracking_path, 'a', newline='') as tracking_fh:
+                writer = csv.writer(tracking_fh)
+                self._intersect_all(works, writer)
+        else:
+            self._intersect_all(works, None)
+
+    def _intersect_all(self, works, writer):
+        for work in works:
+            output_path = os.path.join(self._output_dir, work)
+            os.makedirs(output_path, exist_ok=True)
+            catalogue = tacl.Catalogue()
+            catalogue[work] = work
+            for alt_work in works:
+                if writer is None:
+                    self.process_pair(work, alt_work, catalogue, output_path)
+                else:
+                    self.process_pair_with_tracking(work, alt_work, catalogue,
+                                                    output_path, writer)
+            # Delete output directory if empty.
+            try:
+                os.rmdir(output_path)
+            except OSError:
+                pass
 
     def _get_seen_pairs(self, tracking_filename):
         seen_pairs = {}
@@ -97,23 +106,25 @@ class PairedIntersector:
         return os.path.join(output_path, '{}-{}.csv'.format(
             filename, alt_filename))
 
-    def process_pair(self, work, alt_work, catalogue, output_path,
-                     writer):
+    def process_pair(self, work, alt_work, catalogue, output_path):
+        self._logger.info('Intersecting {} with {}'.format(work, alt_work))
+        catalogue[alt_work] = alt_work
+        results = self._get_results(catalogue)
+        results_filename = self._get_results_filename(output_path, work,
+                                                      alt_work)
+        with open(results_filename, 'w', newline='') as results_fh:
+            results.csv(results_fh)
+        del catalogue[alt_work]
+
+    def process_pair_with_tracking(self, work, alt_work, catalogue,
+                                   output_path, writer):
         if alt_work in self._seen_pairs.get(work, []) or \
            work == alt_work:
                 self._logger.info(
                     'Skipping {} with {} as already done'.format(
                         work, alt_work))
         else:
-            self._logger.info('Intersecting {} with {}'.format(
-                work, alt_work))
-            catalogue[alt_work] = alt_work
-            results = self._get_results(catalogue)
-            results_filename = self._get_results_filename(
-                output_path, work, alt_work)
-            with open(results_filename, 'w', newline='') as results_fh:
-                results.csv(results_fh)
-            del catalogue[alt_work]
+            self.process_pair(work, alt_work, catalogue, output_path)
             self._seen_pairs.setdefault(work, []).append(alt_work)
             self._seen_pairs.setdefault(alt_work, []).append(work)
             self._logger.debug(
